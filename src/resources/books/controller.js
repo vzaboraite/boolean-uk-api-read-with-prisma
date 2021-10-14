445b807e514cc109e6f8d707c26518d7b25e99f3
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-14 22:03:41 +0100

    Build `getOneById` controller for `books` resource.

--- src/resources/books/controller.js
+++ src/resources/books/controller.js
@@ -13,4 +13,23 @@ const getAll = async (req, res) => {
   }
 };
 
-module.exports = { getAll };
+const getOneById = async (req, res) => {
+  const { id } = req.params;
+  const targetId = parseInt(id);
+
+  try {
+    const result = await prisma.book.findUnique({
+      where: {
+        id: targetId,
+      },
+    });
+
+    res.json({ data: result });
+  } catch (error) {
+    console.error({ error: error.message });
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAll, getOneById };
