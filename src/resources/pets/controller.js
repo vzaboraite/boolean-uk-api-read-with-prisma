bb5012e35fdb76ad16cdfe3b827351e1f114d3e6
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-18 09:32:28 +0100

    Build `getPetTypes` controller for `pets` resource.

--- src/resources/pets/controller.js
+++ src/resources/pets/controller.js
@@ -30,7 +30,24 @@ const getOneById = async (req, res) => {
   }
 };
 
+const getPetTypes = async (req, res) => {
+  try {
+    const result = await prisma.pet.findMany({
+      distinct: ["type"],
+      select: {
+        type: true,
+      },
+    });
+    res.json({ data: result });
+  } catch (error) {
+    console.error({ error: error.message });
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getAll,
   getOneById,
+  getPetTypes,
 };
--- src/resources/pets/router.js
+++ src/resources/pets/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getAll, getOneById } = require("./controller");
+const { getAll, getOneById, getPetTypes } = require("./controller");
 
 const router = express.Router();
 
 router.get("/", getAll);
+
+router.get("/types", getPetTypes);
 
 router.get("/:id", getOneById);
 
